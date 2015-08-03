a8b9a85836f4d76015daa7419f830ce8b5aa4d84
emilenriquez <emil.nrqz@gmail.com>
2015-08-03 20:56:38 +0800

    added meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,18 @@ class ApplicationController < ActionController::Base
   def tag_line
   	'Turning caffeine into awesome web and mobile apps'
   end
+
+  def set_default_meta_tags
+    set_meta_tags :canonical   => 'http://emilenriquez.com'
+    set_meta_tags :description => 'Emil Enriquez Developer Profile, Blog, Tutorial and Q&A'
+    set_meta_tags :keywords => 'emilenriquez, Emil Enriquez, Emil Enriquez Developer, Web and Android Developer, Android, Web, Ruby on Rails, emilenriquez tutorial, Ruby Tutorial'
+    set_meta_tags :site_name => 'emilenriquez'
+    
+    set_meta_tags :og => {:title    => 'emilenriquez',
+                          :type     => 'website',
+                          :url      => 'https://emilenriquez.com',
+                          :site      => 'EmilEnriquez',                          
+      }
+
+  end
 end
